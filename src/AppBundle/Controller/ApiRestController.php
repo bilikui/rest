78da4c0a344613c6dc418ef54c444fce51b83dcb
Gustavo Luis <gluis@adifse.com.ar>
2021-02-01 02:02:06 -0300

    Business rules

--- src/AppBundle/Controller/ApiRestController.php
+++ src/AppBundle/Controller/ApiRestController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Route;
+use Symfony\Component\Debug\Exception\OutOfMemoryException;
 
 class ApiRestController extends FOSRestController
 {
@@ -14,21 +15,32 @@ class ApiRestController extends FOSRestController
     {
         $data = array();
 
-        if (is_numeric($min) && is_numeric($max) && $max >= $min) {
-            for ($i = $min; $i <= $max; $i++) {
-                if ($i % 3 == 0 && $i % 5 == 0) {
-                    $data[] = 'FizzBuzz';
-                } elseif ($i % 3 == 0) {
-                    $data[] = 'Fizz';
-                } elseif ($i % 5 == 0) {
-                    $data[] = 'Buzz';
-                } else {
-                    $data[] = (int) $i;
+        // If the minimum and maximum are integers and maximum is greater than or equal to the minimum
+        if ($this->isInteger($min) && $this->isInteger($max) && $max >= $min) {
+            // I prevent with the parameter "maximum difference value between min and max", the out of memory error
+            if ($max - $min <= $this->getParameter('maximum_difference_value_between_min_max')) {
+                for ($i = $min; $i <= $max; $i++) {
+                    if ($i == 0) {
+                        $data[] = (int) $i;
+                    } elseif ($i % 3 == 0 && $i % 5 == 0) {
+                        $data[] = 'FizzBuzz';
+                    } elseif ($i % 3 == 0) {
+                        $data[] = 'Fizz';
+                    } elseif ($i % 5 == 0) {
+                        $data[] = 'Buzz';
+                    } else {
+                        $data[] = (int) $i;
+                    }
                 }
             }
         }
 
         $view = $this->view($data);
         return $this->handleView($view);
+    }
+
+    private function isInteger($value)
+    {
+        return is_numeric($value) && is_int(abs($value)) ? true : false;
     }
 }
--- tests/AppBundle/Controller/ApiRestControllerTest.php
+++ tests/AppBundle/Controller/ApiRestControllerTest.php
@@ -10,32 +10,37 @@ class ApiRestControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $client->request('GET', '/fizzbuzz/1/15');
+        $client->request('GET', '/fizzbuzz/-15/15');
 
         // Check code 200
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
         // Check the format is a json
         $this->assertTrue($client->getResponse()->headers->contains(
-    		'Content-Type', 'application/json'
-		));
+            'Content-Type',
+            'application/json'
+        ));
 
         // Check the expected result
-        $result = '[1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz",13,14,"FizzBuzz"]';
+        $result = '["FizzBuzz",-14,-13,"Fizz",-11,"Buzz","Fizz",-8,-7,"Fizz","Buzz",-4,"Fizz",-2,-1,0,1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz",13,14,"FizzBuzz"]';
         $this->assertEquals($client->getResponse()->getContent(), $result);
     }
 
     public function testIndexErrorsAction()
     {
-    	$client = static::createClient();
+    	$result = '[]';
+
+        $client = static::createClient();
 
         $client->request('GET', '/fizzbuzz/15/1');
-
-        $result = '[]';
 
         $this->assertEquals($client->getResponse()->getContent(), $result);
 
         $client->request('GET', '/fizzbuzz/aa/bb');
+
+        $this->assertEquals($client->getResponse()->getContent(), $result);
+
+        $client->request('GET', '/fizzbuzz/1.5/10.25');
 
         $this->assertEquals($client->getResponse()->getContent(), $result);
     }
